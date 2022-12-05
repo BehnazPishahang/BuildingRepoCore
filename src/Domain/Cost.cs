6871d2d4dd3002f8bb9c710fbee0e89b1c121601
m.abbasi <m.abbasi201@gmail.com>
2022-12-05 18:12:33 +0330

    1401-09-14-CreateTables

--- src/Domain/Cost.cs
+++ src/Domain/Cost.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Domain
 {
-    public class Cost
+    public class Cost:BaseEntity
     {
         public decimal Amount { get; set; }
 
@@ -17,5 +18,17 @@ namespace Domain
         public string ToDate { get; set; }
 
         public decimal CashAmount { get; set; }
+
+        public virtual Building Building { get; set; }
+
+        public Guid BuildingId { get; set; }
+
+        public virtual CostType CostType { get; set; }
+
+        public Guid CostTypeId { get; set; }
+
+        public virtual ObjectState ObjectState { get; set; }
+
+        public Guid ObjectStateId { get; set; }
     }
 }
