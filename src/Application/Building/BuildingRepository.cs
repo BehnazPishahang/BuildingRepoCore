a0e47a8da6f6e6d3e2e76fde5cd3ac8042570f0d
b.pishahang <b.pishahang@gmail.com>
2022-12-07 11:30:53 +0330

    commitRepo

---
+++ src/Application/Building/BuildingRepository.cs
@@ -0,0 +1,21 @@
+﻿using Application.Common;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Building
+{
+    public class BuildingRepository : GenericRepository<Domain.Building.Building>, Application.Building.IBuildingRepository
+    {
+
+        public BuildingRepository(DataContext context) : base(context)
+        {
+
+        }
+
+    }
+}
---
+++ src/Application/Building/IBuildingRepository.cs
@@ -0,0 +1,17 @@
+﻿using Application.Common;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Building
+{
+    public interface IBuildingRepository: IGenericRepository<Domain.Building.Building>
+    {
+
+        
+
+    }
+}
